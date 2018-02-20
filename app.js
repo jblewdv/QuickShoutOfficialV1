b92cf62e1c6de24adf89836b8c0c9c248c2275b9
Joshua Blew <JoshuaBlew@Joshuas-MacBook-Pro.local>
2018-02-19 21:09:42 -0600

    Add Helmet

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
+var helmet = require('helmet');
 // <==============================================================>
 
 
